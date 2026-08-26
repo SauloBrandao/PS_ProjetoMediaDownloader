3eed6a812f707aab5adcb3d5085ec1ca9280ae0d
Saulo Pinto <saulobf8@gmail.com>
2026-08-26 20:17:42 -0300

    feat#2 -> Importando yt-dlp para o código

---
+++ downloader/downloader.py
@@ -0,0 +1,10 @@
+import yt_dlp
+
+def baixar_mp4(url: str) -> None:
+    opcoes = {
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+    }
+
+    with yt_dlp.YoutubeDL(opcoes) as ydl:
+        ydl.download([url])
