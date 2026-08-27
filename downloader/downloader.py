c2916458c799bcefbbcbfef8216960fe4bd0b5e9
SauloBrandao <2023211510080@iesp.edu.br>
2026-08-27 02:43:04 -0300

    feat#3 -> Corrigindo erro de download de playlist, somente baixando videos unicos

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -4,6 +4,8 @@ def baixar_mp4(url: str) -> None:
     opcoes = {
         "format": "bestvideo+bestaudio/best",
         "merge_output_format": "mp4",
+
+        "noplaylist": True,
     }
 
     with yt_dlp.YoutubeDL(opcoes) as ydl:
