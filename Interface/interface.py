2e932132ca2c0cf6b750373e53ee7c12e522688b
SauloBrandao <2023211510080@iesp.edu.br>
2026-09-01 23:40:59 -0300

    Feat#3.0 -> Configurando interface e opção de selecionar destino

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,8 +1,24 @@
 import threading
-from _pyrepl import commands
 
 import customtkinter as ctk
-from downloader.downloader import baixar_mp4
+from tkinter import filedialog
+from serviços.downloader.downloader import baixar_mp4
+
+def inificar_download(): # -> Criando Thread para evitar que o tkinter crashe
+    url = url_entrada.get()
+
+    threading.Thread(
+        target=baixar_mp4,
+        args=(url,),
+        daemon=True
+    ).start()
+
+def selecionar_pasta(): # -> criando função para selecionar diretorio
+    caminho = filedialog.askdirectory()
+
+    if caminho:
+        print(caminho)
+
 
 ctk.set_appearance_mode("system")
 
@@ -18,13 +34,15 @@ titulo = ctk.CTkLabel( # -> Titulo do Software
 )
 titulo.pack(pady=30) # -> espaço padrão 30 px
 
-url_entry = ctk.CTkEntry(
+
+
+url_entrada = ctk.CTkEntry(
     app,
     width=450,
     placeholder_text="Cole o link do vídeo",
     font=ctk.CTkFont(family="Indie Flower")
 )
-url_entry.pack(pady=10) # -> padrão é 10 px
+url_entrada.pack(pady=10) # -> padrão é 10 px
 
 formato = ctk.CTkSegmentedButton(
     app,
@@ -37,15 +55,6 @@ formato = ctk.CTkSegmentedButton(
 formato.set("MP4")
 formato.pack(pady=10)
 
-def inificar_download(): # -> Criando Thread para evitar que o tkinter crashe
-    url = url_entry.get()
-
-    threading.Thread(
-        target=baixar_mp4,
-        args=(url,),
-        daemon=True
-    ).start()
-
 botao_download = ctk.CTkButton(
     app,
     text="Baixar",
@@ -56,5 +65,12 @@ botao_download = ctk.CTkButton(
     command=inificar_download
 )
 botao_download.pack(pady=20)
+
+botao_pasta = ctk.CTkButton(
+    app,
+    text="Selecionar pasta",
+    command=selecionar_pasta
+)
+botao_pasta.pack(padx=20, pady=20)
 
 app.mainloop()
